e58c78d7d933e2a90155f2d0f42c84dfae3b2491
jobtarzak <jobtarzak@gmail.com>
2014-07-20 07:30:42 -0400

    add 3 links css and front-end

--- myJS/the-westScript.js
+++ myJS/the-westScript.js
@@ -24,3 +24,13 @@ var p = document.createElement('P');
 p.innerHTML = "ansodadsa";
 console.log(p.innerHTML);
 uiCC.appendChild(p);
+
+var objArr = WestUi.FriendsBar.friendsBarUi.friendsBar.getAllPlayers();
+var friendsNames = [], j = 0;
+
+for (j = 0; j < objArr.length; j++)
+friendsNames[j] = objArr[j].name;
+friendsNames.sort();
+var rawList = friendsNames.join("\n");
+console.log(rawList);
+console.log(rawList.length);
